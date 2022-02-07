6dd5dda4b52a9e993e4152e05f5676249268ccc0
Haidarali Masu <haidaralimasu123@gmail.com>
2022-02-07 06:56:38 +0530

    initial commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -83,7 +83,7 @@ function Navbar() {
               <li className="nav-btn">
                 {button ? (
                   <a
-                    href="https://etherscan.io"
+                    href="https://etherscan.io/address/0x4A4b02C53aA9382808C2c033455471d231535352"
                     target="blank"
                     className="btn-link"
                   >
